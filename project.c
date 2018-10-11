c78476a5956ccd17690ec44fdde220311eee2715
STARTERGATE <32726809+startergate@users.noreply.github.com>
2018-10-11 19:44:23 +0900

    헉헉

--- project.c
+++ project.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <Windows.h>
-void item();
-void roomDisplayer(Room (*apart)[5]);
+
 int sum = 0;
+typedef struct cardInfo {
+	int parse1, parse2, parse3, parse4, cvc, cardNumber, passcode;
+} Card;
+
+typedef struct passbook {
+	char number[15];
+	int passcode;
+} Bank;
 
 typedef struct paymentInfo {
 	Card card;
@@ -11,44 +18,68 @@ typedef struct paymentInfo {
 } Pay;
 
 typedef struct room {
-	int lastDate;
+	unsigned int lastDate;
 	int productUse[6];
 	Pay payment;
 } Room;
 
-typedef struct passbook {
-	char number[15];
-	int passcode;
-} Bank;
+void roomDisplayer(Room (*apart)[5]);
+void item(Room (*apart)[5], int floor, int room);
+void pay(Room(*apart)[5], int floor, int room);
 
-typedef struct cardInfo {
-	int parse1, parse2, parse3, parse4, cvc, cardNumber, passcode;
-} Card;
 int main() {
-	Room apart[15][5];
+	Room apart[15][5] = { 0 };
 	int an, floor, room, length, sw, day = 0;
-again:
-	do
+	while (1)
 	{
-
 		while (1)
 		{
+			sum = 0;
 			system("cls");
 			printf("%d일차입니다.\n", day + 1);
 			roomDisplayer(apart);
-			printf("\n동행관 관리자 예약 콘솔...\n층과 호실번호를 입력하세요.\n다음날로 넘어가려면 -1를 입력하세요.\n프로그램을 종료하려면 -2를 입력하세요.\n");
+			printf("\n동행관 예약 관리자 콘솔...\n층과 호실번호를 입력하세요.\n다음날로 넘어가려면 -1를 입력하세요.\n예약 기간을 연장하려면 -2를 입력하세요.\n프로그램을 종료하려면 -3를 입력하세요.\n");
 		floor:
 			scanf("%d", &floor);
 			if (floor == -1)
 			{
 				day = day + 1;
-				goto again;
+				continue;
 			}
-			if (floor == -2) goto EXIT_POINT;
+			if (floor == -2)
+			{
+				system("cls");
+			renew:
+				printf("층과 호실번호를 입력하세요.\n빠져나가려면 -1을 입력하세요.\n:");
+				scanf("%d", &floor);
+				if (floor == -1)
+				{
+					continue;
+				}
+				scanf("%d", &room);
+				if (!apart[floor - 1][room - 1].lastDate) {
+					system("cls");
+					printf("예약된 방이 아닙니다. ");
+					goto renew;
+				}
+				system("cls");
+				printf("변경할 날짜를 입력하세요.\n:");
+				scanf("%d", &length);
+				apart[floor - 1][room - 1].lastDate += length;
+				sum += length * 220000;
+				item(apart, floor, room);
+				pay(apart, floor, room);
+			}
+			if (floor == -3) goto EXIT_POINT;
 			if (floor < 1 || floor > 15) goto floor;
 		room:
 			scanf("%d", &room);
-			if (room < 1 || room > 5) goto room;
+			if (room < 1 || room > 5) {
+				printf("다시 입력하세요. 빠져나가려면 -1을 입력하세요.\n");
+				scanf("%d", &floor);
+				if (floor == -1) continue;
+				goto room;
+			}
 			if (apart[floor - 1][room - 1].lastDate) {
 				system("cls");
 				printf("이미 예약된 방입니다. 다른 방을 선택하세요.\n");
@@ -64,78 +95,10 @@ again:
 			scanf("%d", &an);
 			if (an == 1)
 			{
-				item();
+				item(apart, floor, room);
 			}
 		pay:
-			system("cls");
-			printf("%d원을 결제합니다.\n결제 방법을 선택하세요.\n", sum);
-			printf("1.현금 2.신용/체크카드 3.계좌 이체\n");
-			scanf("%d", &sw);
-			switch (sw)
-			{
-			case 1:
-				printf("현금을 투입해주세요.\n");
-			bill:
-				scanf("%d", &sw);
-				if (sw == sum) break;
-				else if (sum < sw)
-				{
-					sum -= sw;
-					printf("%d원이 환급되었습니다.", -sum);
-					break;
-				}
-				else
-				{
-					sum -= sw;
-					printf("%d원을 더 투입하세요.", sum);
-					goto bill;
-				}
-			case 2:
-			{
-				printf("┌───────────────────────────────────────────┐\n");
-				printf("│////// 결제하실 카드를 선택해주세요//////  │\n");
-				printf("│ 1.신한카드                                │\n");
-				printf("│ 2.현대카드                                │\n");
-				printf("│ 3.삼성카드                                │\n");
-				printf("│ 4.국민카드                                │\n");
-				printf("│ 5.하나카드                                │\n");
-				printf("│ 6.GSM카드                                 │\n");
-				printf("└───────────────────────────────────────────┘\n");
-				scanf("%d", &apart[floor - 1][room - 1].payment.card.cardNumber);
-				printf("카드 번호를 입력하세요.\n예) 1234 5678 2345 5678\n");
-				scanf("%d %d %d %d", &apart[floor - 1][room - 1].payment.card.parse1, &apart[floor - 1][room - 1].payment.card.parse2, &apart[floor - 1][room - 1].payment.card.parse3, &apart[floor - 1][room - 1].payment.card.parse4);
-			code:
-				printf("비밀번호 앞 2자리를 입력하세요.\n");
-				int pcode;
-				scanf("%d", &pcode);
-				if (pcode < 0 || pcode > 99)
-					goto code;
-				apart[floor - 1][room - 1].payment.card.passcode = pcode;
-				printf("CVC값을 입력하세요.\n");
-				scanf("%d", &apart[floor - 1][room - 1].payment.card.cvc);
-			}
-			break;
-			case 3:
-				account:
-				printf("계좌번호 11 ~ 14자리를 입력하세요.\n:");
-				char nu[15];
-				getchar();
-				scanf("%s", nu);
-				for (int i = 0; i < 11; i++)
-				{
-					if (nu[i] == '\0')
-					{
-						printf("다시 입력해주세요.\n");
-						goto account;
-					}
-				}
-				strcpy(apart[floor - 1][room - 1].payment.bank.number, nu);
-				break;
-				printf("비밀번호를 입력하세요.\n:");
-				scanf("%d", apart[floor - 1][room - 1].payment.bank.passcode);
-			default:
-				goto pay;
-			}
+			pay(apart, floor, room);
 			system("cls");
 		}
 		day++;
@@ -155,7 +118,7 @@ again:
 		}
 		Sleep(3000);
 		system("cls");
-	} while (1);
+	}
 EXIT_POINT:
 	system("cls");
 	roomDisplayer(apart);
@@ -168,7 +131,7 @@ EXIT_POINT:
 	printf("///////////////////////////////////////GSM\n");
 	return 0;
 }
-void item()
+void item(Room (*apart)[5], int floor, int room)
 {
 	int pay;
 	while (1)
@@ -185,23 +148,11 @@ void item()
 
 		printf("구매하실 상품의 번호를 입력해주세요:");
 		scanf("%d", &pay);
-
-		if (pay == 1 || pay == 5)
-		{
-			sum += 1000;
-		}
-		else if (pay == 2 || pay == 3 || pay == 4)
-		{
-			sum += 700;
-		}
-		else if (pay == 5)
-		{
-			sum += 1000;
-		}
-		else if (pay == 6)
-		{
-			sum += 15000;
-		}
+		apart[floor - 1][room - 1];
+		if (pay == 1 || pay == 5) sum += 1000;
+		else if (pay == 2 || pay == 3 || pay == 4) sum += 700;
+		else if (pay == 5) sum += 1000;
+		else if (pay == 6) sum += 15000;
 		else if (pay == 7)
 		{
 			system("cls");
@@ -219,5 +170,78 @@ void roomDisplayer(Room (*apart)[5]) {
 			else printf("□ ");
 		}
 		printf("\n");
+	}
+}
+
+void pay(Room(*apart)[5], int floor, int room) {
+	int sw;
+	system("cls");
+	printf("%d원을 결제합니다.\n결제 방법을 선택하세요.\n", sum);
+	printf("1.현금 2.신용/체크카드 3.계좌 이체\n");
+	scanf("%d", &sw);
+	switch (sw)
+	{
+	case 1:
+		printf("현금을 투입해주세요.\n");
+	bill:
+		scanf("%d", &sw);
+		if (sw == sum) break;
+		else if (sum < sw)
+		{
+			sum -= sw;
+			printf("%d원이 환급되었습니다.", -sum);
+			break;
+		}
+		else
+		{
+			sum -= sw;
+			printf("%d원을 더 투입하세요.", sum);
+			goto bill;
+		}
+	case 2:
+	{
+		printf("┌───────────────────────────────────────────┐\n");
+		printf("│ ////// 결제하실 카드를 선택해주세요////// │\n");
+		printf("│ 1.신한카드                                │\n");
+		printf("│ 2.현대카드                                │\n");
+		printf("│ 3.삼성카드                                │\n");
+		printf("│ 4.국민카드                                │\n");
+		printf("│ 5.하나카드                                │\n");
+		printf("│ 6.GSM카드                                 │\n");
+		printf("└───────────────────────────────────────────┘\n");
+		scanf("%d", &apart[floor - 1][room - 1].payment.card.cardNumber);
+		printf("카드 번호를 입력하세요.\n예) 1234 5678 2345 5678\n");
+		scanf("%d %d %d %d", &apart[floor - 1][room - 1].payment.card.parse1, &apart[floor - 1][room - 1].payment.card.parse2, &apart[floor - 1][room - 1].payment.card.parse3, &apart[floor - 1][room - 1].payment.card.parse4);
+	code:
+		printf("비밀번호 앞 2자리를 입력하세요.\n");
+		int pcode;
+		scanf("%d", &pcode);
+		if (pcode < 0 || pcode > 99)
+			goto code;
+		apart[floor - 1][room - 1].payment.card.passcode = pcode;
+		printf("CVC값을 입력하세요.\n");
+		scanf("%d", &apart[floor - 1][room - 1].payment.card.cvc);
+	}
+	break;
+	case 3:
+	account:
+		printf("계좌번호 11 ~ 14자리를 입력하세요.\n:");
+		char nu[15];
+		getchar();
+		scanf("%s", nu);
+		for (int i = 0; i < 11; i++)
+		{
+			if (nu[i] == '\0')
+			{
+				printf("다시 입력해주세요.\n");
+				goto account;
+			}
+		}
+		strcpy(apart[floor - 1][room - 1].payment.bank.number, nu);
+		break;
+		printf("비밀번호를 입력하세요.\n:");
+		scanf("%d", &apart[floor - 1][room - 1].payment.bank.passcode);
+	default:
+		pay(apart, floor, room);
 	}
 }
